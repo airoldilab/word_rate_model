cb1e620c0d1344431e4d7e41124d6bbc6bc12426
jbischof <jbischof1@gmail.com>
2013-08-27 22:39:10 -0700

    Improved rating distribution plot in Lattice

--- amazon_turk/parse_aturk_coherence_results.R
+++ amazon_turk/parse_aturk_coherence_results.R
@@ -1,4 +1,5 @@
-# Inputs
+## Inputs
+library("lattice")
 output.dir <- "/home/jbischof/Project_output/word_rate_model/aturk_output/"
 ntopics.use <- c(10,25,50,100)
 r <- 1
@@ -7,11 +8,14 @@ relpref.success.mat <- matrix(NA,ncol=nmodels+1,nrow=length(ntopics.use))
 rownames(relpref.success.mat) <- ntopics.use
 n.relpref.mat <- relpref.success.mat
 
+model.names <-c("dtr_frex","lda_freq","lda_frex")
+
 topic.rate.mat <- relpref.success.mat[,1:3]
 n.rate.mat <- topic.rate.mat
 
 binom.ci.list <- list()
 rate.ci.list <- list()
+topic.res.df <- data.frame(x=c())
 
 
 for(ntopics in ntopics.use){
@@ -26,17 +30,34 @@ for(ntopics in ntopics.use){
   header <- colnames(tab.res)
   pos.qid <- grep(pattern="Input.topic[0-9]{2}id",x=header)
   pos.qans <- grep(pattern="Answer.topic[0-9]{2}$",x=header)
+  pos.topicsum <- grep(pattern="Input.topic[0-9]{2}$",x=header)
   pos.relans <- grep(pattern="Answer.relpref",x=header)
-  
+
+  ## ## Create variable to uniquely identify model-topic comb
+  ## nquest <- length(pos.qid)
+  ## for(j in 1:nquest){
+  ##   qid <- tab.res[,pos.qid[j]]
+  ##   qid.model <- sapply(qid,function(string){
+  ##     strsplit(string,split="_")[[1]][1]})
+  ##   topic.sum <- tab.res[,pos.qans[j]]
+    
+  ## }
+                     
   ##print(header[pos.qid])
   ##print(header[pos.qans])
   ##print(header[pos.relans])
+
+ 
   
   qid.mat <- tab.res[,pos.qid]
   qid.mat.relpref <- cbind(qid.mat,"No preference")
   score.mat <- tab.res[,pos.qans]
   relans <- tab.res[,pos.relans]
   relans[relans == 999] <- 3
+
+  topic.sum.mat.raw <- apply(tab.res[,pos.topicsum],2,as.character)
+  topic.sum.mat <- t(sapply(1:njobs,function(x){
+    topic.sum.mat.raw[x,][order(qid.mat[x,])]}))
   
   ## Get absolute score result
   names.order <- as.vector(as.matrix(qid.mat[1,][order(qid.mat[1,])]))
@@ -59,6 +80,33 @@ for(ntopics in ntopics.use){
     colnames(n.rate.mat) <- colnames(topic.rate.mat) <-
       names(mean.agg.res)
   }
+
+  ## Aggregate results by topic
+  nquest <- nmodels*r
+  ## Get average results by topic
+  topic.res <- sapply(1:nquest,function(pos){
+    tapply(worker.res[,pos],topic.sum.mat[,pos],FUN=mean,na.rm=TRUE)})
+  if(is.list(topic.res)){
+    topic.res <- sapply(topic.res,function(vec){
+      out <- rep(NA,ntopics)
+      out[1:length(vec)] <- vec
+      return(out)
+    })
+  }
+
+  ## Label results
+  topic.res.label <- rep(model.names,each=ntopics)
+  topic.res.df.add <- data.frame(topic.score=as.vector(topic.res),
+                                  topic.label=topic.res.label,
+                                  ntopics=ntopics)
+  
+  ## Append results to topic.res.df
+  topic.res.df <- rbind(topic.res.df,topic.res.df.add)
+  
+  
+  ## colnames(topic.res) <- names.order
+  ## rownames(topic.res) <- NULL
+  ## topic.res.list[[as.character(ntopics)]] <- topic.res
   
   ## Get relative pref result
   count.nona.relpref <- length(which(!is.na(relans)))
@@ -94,10 +142,10 @@ for(ntopics in ntopics.use){
   
 }
 
+
 n.relpref.mat
 relpref.success.mat
 binom.ci.list
-
 
 
 binom.ci.vec <- unlist(binom.ci.list)
@@ -171,3 +219,30 @@ for(j in 1:nmodels){
 }
 legend(x="bottomleft",legend=pretty.model.names,lty=model.lty,pch=model.pch)
 dev.off()
+
+
+## Plot the distribution of ratings for topics
+## Convert ntopics into factor variable
+topic.res.df$ntopics <- factor(topic.res.df$ntopics,
+                               levels=as.character(rev(ntopics.use)))
+## Use Lattice to get grid of histograms
+file.pdf <- paste0(output.dir,"tc_rate_dist.pdf")
+pdf(file.pdf,width=8,height=8)
+histogram(~topic.score|topic.label+ntopics,data=topic.res.df,
+          xlab="Average topic rating",col="white")
+dev.off()
+
+## ## Need to put axis labels on side of plot
+## file.pdf <- paste0(output.dir,"tc_rate_dist.pdf")
+## pdf(file.pdf,width=8,height=8)
+## par(mfrow=c(4,3),mar=rep(2,4))
+## for(ntopics in ntopics.use){
+##   res.mat <- topic.res.list[[as.character(ntopics)]]
+##   for(pos in 1:nmodels){
+##     hist(res.mat[,pos],main="",
+##          ##main=ifelse(ntopics==10,pretty.model.names[pos],""),
+##          breaks=seq(1,3,length.out=(ntopics/4)+1),#[-c(1,ntopics/4)],
+##          freq=TRUE,xaxt="n",xlab="",ylab="")
+##   }
+## }
+## dev.off()
